d545e58e2c08a72c4b18b3e7143d6d7e7a50f708
gbs <gengbushuang@apusapps.com>
2019-10-08 00:22:42 +0800

    dnf

--- protobuf-test/src/main/java/match/engine/TargetingIndexBuilder.java
+++ protobuf-test/src/main/java/match/engine/TargetingIndexBuilder.java
@@ -13,13 +13,22 @@ public class TargetingIndexBuilder {
     //key-->conj的MD5
     //value-->conjId
     private Map<String, Integer> conjFingerMap = new HashMap<>();
-    private List<TargetingIndexOuterClass.ConjunctionIndex> conjIndexVec = new ArrayList<>();
+    private List<TargetingIndexOuterClass.ConjunctionIndex.Builder> conjIndexVec = new ArrayList<>();
+    private List<TargetingIndexOuterClass.ConjunctionIndex.Builder> newConjIndexVec;
 
-    private Map<TargetingIndexOuterClass.Interval,Integer> intervalMap = new HashMap<>();
+
+    private Map<TargetingIndexOuterClass.Interval, Integer> intervalMap = new HashMap<>();
     private List<TargetingIndexOuterClass.Interval> intervalVec = new ArrayList<>();
 
 
-    private List<TargetingIndexOuterClass.TokenIndex> tokenIndexVec = new ArrayList<>();
+    private List<TargetingIndexOuterClass.TokenIndex.Builder> tokenIndexVec = new ArrayList<>();
+
+    private List<TargetingIndexOuterClass.AidIndex> aidIndexVec = new ArrayList<>();
+
+    private List<TargetingIndexOuterClass.AdIndex> adIndexVec = new ArrayList<>();
+
+
+    private int[] conjIdRemapping;
 
 
     public void build(List<TargetingBe.TargetingBE> beVec) {
@@ -27,6 +36,54 @@ public class TargetingIndexBuilder {
             TargetingBe.TargetingBE be = beVec.get(i);
             int localId = i;
             processBe(be, localId);
+        }
+
+        idRemapping();
+        reArrangeData();
+    }
+
+    private void idRemapping() {
+        int max_hit_count = 0;
+        for (TargetingIndexOuterClass.ConjunctionIndex.Builder conjIndexB : conjIndexVec) {
+            TargetingIndexOuterClass.ConjunctionIndex index = conjIndexB.build();
+            if (index.getHitCount() > max_hit_count) {
+                max_hit_count = index.getHitCount();
+            }
+        }
+        List<List<Integer>> hitList = new ArrayList<>(max_hit_count + 1);
+        for (int i = 0; i < max_hit_count + 1; i++) {
+            hitList.add(new ArrayList<>());
+        }
+//        int[][] hitList = new int[max_hit_count + 1][max_hit_count + 1];
+        for (int i = 0; i < conjIndexVec.size(); i++) {
+            TargetingIndexOuterClass.ConjunctionIndex index = conjIndexVec.get(i).build();
+            int hitCount = index.getHitCount();
+            hitList.get(hitCount).add(i);
+        }
+
+        int newId = 0;
+        conjIdRemapping = new int[conjIndexVec.size()];
+        for (List<Integer> slice : hitList) {
+            for (Integer oldId : slice) {
+                conjIdRemapping[oldId.intValue()] = newId;
+                newId++;
+            }
+        }
+    }
+
+    private void reArrangeData() {
+        //
+        newConjIndexVec = new ArrayList<>(conjIndexVec.size());
+        for (int i = 0; i < conjIndexVec.size(); i++) {
+            int newId = conjIdRemapping[i];
+            newConjIndexVec.set(newId, conjIndexVec.get(i));
+        }
+        //
+        for (TargetingIndexOuterClass.TokenIndex.Builder tokenIndexB : tokenIndexVec) {
+            for (TargetingIndexOuterClass.TokenIndex.ConjunctionHit.Builder conjHitB: tokenIndexB.getConjunctionHitBuilderList()) {
+                int oldId = conjHitB.getConjunctionId();
+                conjHitB.setConjunctionId(conjIdRemapping[oldId]);
+            }
         }
     }
 
@@ -40,17 +97,18 @@ public class TargetingIndexBuilder {
         for (TargetingBe.TargetingDNF.Conjunction conj : conjunctionList) {
             String finger = calcConjFinger(conj);
             System.out.println(finger);
-            TargetingIndexOuterClass.ConjunctionIndex conjIndex;
+            TargetingIndexOuterClass.ConjunctionIndex.Builder conjIndexB = TargetingIndexOuterClass.ConjunctionIndex.newBuilder();
             Integer conjId;
+
             boolean conjExist = conjFingerMap.containsKey(finger);
             if (conjExist) {
                 conjId = conjFingerMap.get(finger);
-                conjIndex = conjIndexVec.get(conjId);
+                conjIndexB = conjIndexVec.get(conjId);
             } else {
                 conjId = conjIndexVec.size();
                 conjFingerMap.put(finger, conjId);
-                conjIndex = TargetingIndexOuterClass.ConjunctionIndex.newBuilder().build();
-                conjIndexVec.add(conjIndex);
+                conjIndexB = TargetingIndexOuterClass.ConjunctionIndex.newBuilder();
+                conjIndexVec.add(conjIndexB);
             }
 
             List<TargetingBe.Predicate> preVec = new ArrayList<>();
@@ -63,9 +121,21 @@ public class TargetingIndexBuilder {
                     preVec.add(predicate);
                 }
             }
-
+            //
             processConjunction(localId, conjId, conjExist, preVec, notPreVec, ad_token_set);
+            //
+            conjIndexB.setHitCount(preVec.size());
+            conjIndexB.addLocalIds(localId);
         }
+        //
+        aidIndexVec.add(TargetingIndexOuterClass.AidIndex.newBuilder().setAid(be.getAid()).setLocalId(localId).build());
+        TargetingIndexOuterClass.AdIndex.Builder adIndexB = TargetingIndexOuterClass.AdIndex.newBuilder();
+        adIndexB.setAid(be.getAid());
+        adIndexB.setAdinfoId(localId);
+        for (Integer tokenId : ad_token_set.keySet()) {
+            adIndexB.addTokenId(tokenId);
+        }
+        adIndexVec.add(adIndexB.build());
     }
 
     private void processConjunction(int localId, Integer conjId, boolean conjExist, List<TargetingBe.Predicate> preVec, List<TargetingBe.Predicate> notPreVec, Map<Integer, Boolean> ad_token_set) {
@@ -79,7 +149,7 @@ public class TargetingIndexBuilder {
                 if (value.getType() == TargetingBe.Predicate.Value.Type.ID) {
                     intervalB
                             .setBegin(value.getId())
-                            .setEnd(value.getId()+1).build();
+                            .setEnd(value.getId() + 1).build();
 
                     adHitB
                             .setLocalId(localId)
@@ -94,42 +164,72 @@ public class TargetingIndexBuilder {
                             .setIsId(false).build();
                 }
 
-                Tuble<Integer, TargetingIndexOuterClass.TokenIndex> tuble = getToken(intervalB.build());
-                if(!conjExist){
+                Tuble<Integer, TargetingIndexOuterClass.TokenIndex.Builder> tuble = getToken(intervalB.build());
+                if (!conjExist) {
+
                     TargetingIndexOuterClass.TokenIndex.ConjunctionHit conjunctionHit = TargetingIndexOuterClass.TokenIndex.ConjunctionHit.newBuilder()
                             .setConjunctionId(conjId)
                             .setPredicateId(pid).build();
 
-                    TargetingIndexOuterClass.TokenIndex.newBuilder(tuble._2())
-                            .addConjunctionHit(conjunctionHit);
-
+                    tuble._2().addConjunctionHit(conjunctionHit);
                 }
 
                 boolean ok = ad_token_set.containsKey(tuble._1());
-                if(!ok){
-                    ad_token_set.put(tuble._1(),true);
-                    TargetingIndexOuterClass.TokenIndex.newBuilder().addAdHit(adHitB);
+                if (!ok) {
+                    ad_token_set.put(tuble._1(), true);
+                    tuble._2().addAdHit(adHitB);
                 }
+            }
+        }
+
+        if (conjExist) {
+            return;
+        }
+
+        for (int p = 0; p < notPreVec.size(); p++) {
+            int pid = p + preVec.size();
+            TargetingBe.Predicate predicate = notPreVec.get(p);
+            for (TargetingBe.Predicate.Value value : predicate.getValueList()) {
+                TargetingIndexOuterClass.Interval.Builder intervalB = TargetingIndexOuterClass.Interval.newBuilder();
+                if (value.getType() == TargetingBe.Predicate.Value.Type.ID) {
+                    intervalB
+                            .setBegin(value.getId())
+                            .setEnd(value.getId() + 1).build();
+                } else if (value.getType() == TargetingBe.Predicate.Value.Type.RANGE) {
+                    intervalB
+                            .setBegin(value.getRange().getBegin())
+                            .setEnd(value.getRange().getEnd()).build();
+                }
+
+                Tuble<Integer, TargetingIndexOuterClass.TokenIndex.Builder> tuble = getToken(intervalB.build());
+                tuble._2().addConjunctionHit(
+                        TargetingIndexOuterClass
+                                .TokenIndex
+                                .ConjunctionHit
+                                .newBuilder()
+                                .setConjunctionId(conjId)
+                                .setPredicateId(pid).build()
+                );
             }
         }
     }
 
-    public Tuble<Integer,TargetingIndexOuterClass.TokenIndex> getToken(TargetingIndexOuterClass.Interval interval){
-        TargetingIndexOuterClass.TokenIndex tokenIndex;
+    public Tuble<Integer, TargetingIndexOuterClass.TokenIndex.Builder> getToken(TargetingIndexOuterClass.Interval interval) {
+        TargetingIndexOuterClass.TokenIndex.Builder tokenIndexB;
         Integer tokenId;
         boolean ok = intervalMap.containsKey(interval);
-        if(!ok){
+        if (!ok) {
             tokenId = intervalVec.size();
             intervalVec.add(interval);
-            intervalMap.put(interval,tokenId);
-            tokenIndex = TargetingIndexOuterClass.TokenIndex.newBuilder().build();
+            intervalMap.put(interval, tokenId);
+            tokenIndexB = TargetingIndexOuterClass.TokenIndex.newBuilder();
 
-            tokenIndexVec.add(tokenIndex);
-        }else{
+            tokenIndexVec.add(tokenIndexB);
+        } else {
             tokenId = intervalMap.get(interval);
-            tokenIndex = tokenIndexVec.get(tokenId);
+            tokenIndexB = tokenIndexVec.get(tokenId);
         }
-        return new Tuble<Integer,TargetingIndexOuterClass.TokenIndex>(tokenId,tokenIndex);
+        return new Tuble<Integer, TargetingIndexOuterClass.TokenIndex.Builder>(tokenId, tokenIndexB);
     }
 
 
