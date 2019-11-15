9234df015c0db5144dbd082da1579eafa51aa495
gengbushuang <gengbushuang@163.com>
2019-11-15 19:36:15 +0800

    APT编译期注解

---
+++ java-annotation/src/main/java/com/annotation/processor/MyAPTProcessor.java
@@ -0,0 +1,35 @@
+package com.annotation.processor;
+//这个主要自动生成 java spi META-INF/services
+
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+@AutoService(Processor.class)
+public class MyAPTProcessor extends AbstractProcessor {
+
+
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        processingEnv.getFiler();
+        processingEnv.getMessager();
+        processingEnv.getElementUtils();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+}
