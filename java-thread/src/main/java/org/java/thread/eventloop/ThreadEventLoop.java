6ec2aa748c3619364020bfb8e5c2e4fc45c5cfe8
gengbushuang <gengbushuang@163.com>
2019-12-18 15:29:44 +0800

    eventloop

--- java-thread/src/main/java/org/java/thread/eventloop/ThreadEventLoop.java
+++ java-thread/src/main/java/org/java/thread/eventloop/ThreadEventLoop.java
@@ -3,13 +3,27 @@ package org.java.thread.eventloop;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 
 public abstract class ThreadEventLoop extends AbstractExecutorService {
-
+    //没有开启
+    private static final int ST_NOT_STARTED = 1;
+    //进行中
+    private static final int ST_STARTED = 2;
+    //停止
+    private static final int ST_SHUTTING_DOWN = 3;
+    //关闭
+    private static final int ST_SHUTDOWN = 4;
+    //结束
+    private static final int ST_TERMINATED = 5;
+    //原子性字段的操作，可以根据反射对类里面的volatile int字段进行原子更新
+    private static final AtomicIntegerFieldUpdater<ThreadEventLoop> STATE_UPDATE = AtomicIntegerFieldUpdater.newUpdater(ThreadEventLoop.class, "state");
 
     private final Queue<Runnable> taskQueue;
 
     private final int maxPendingTasks;
+
+    private volatile int state = ST_NOT_STARTED;
 
     private final RejectedExecutionHandler rejectedHandler;
 
@@ -36,12 +50,12 @@ public abstract class ThreadEventLoop extends AbstractExecutorService {
 
     @Override
     public boolean isShutdown() {
-        return false;
+        return state >= ST_SHUTDOWN;
     }
 
     @Override
     public boolean isTerminated() {
-        return false;
+        return state == ST_TERMINATED;
     }
 
     @Override
@@ -54,6 +68,9 @@ public abstract class ThreadEventLoop extends AbstractExecutorService {
         if (task == null) {
             throw new NullPointerException("task");
         }
+        this.addTask(task);
+
+        this.startThread();
     }
 
     abstract protected void run();
@@ -85,5 +102,35 @@ public abstract class ThreadEventLoop extends AbstractExecutorService {
             // Ignore
         }
         return task;
+    }
+
+    private void startThread() {
+        if (state == ST_NOT_STARTED) {
+            if (STATE_UPDATE.compareAndSet(this, ST_NOT_STARTED, ST_STARTED)) {
+                try {
+                    doStartThread();
+                } catch (Throwable e) {
+                    STATE_UPDATE.set(this, ST_NOT_STARTED);
+                }
+
+            }
+        }
+    }
+
+    private void doStartThread() {
+        new Thread(() -> {
+            try {
+                ThreadEventLoop.this.run();
+            } catch (Throwable e) {
+
+            } finally {
+                for (; ; ) {
+                    int oldState = this.state;
+                    if (oldState >= ST_SHUTTING_DOWN || STATE_UPDATE.compareAndSet(this, oldState, ST_SHUTTING_DOWN)) {
+                        break;
+                    }
+                }
+            }
+        }).start();
     }
 }
