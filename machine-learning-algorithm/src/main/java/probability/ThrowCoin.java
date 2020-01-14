384b12270ed63b33a2f09c2cefe18f59279a48cd
gengbushuang <gengbushuang@163.com>
2020-01-14 20:03:25 +0800

    抛硬币

---
+++ machine-learning-algorithm/src/main/java/probability/ThrowCoin.java
@@ -0,0 +1,27 @@
+package probability;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ThrowCoin {
+
+    public void flipCoin(int n) {
+        Map<Integer, Integer> m = Stream.generate(() -> (int) (Math.random() * 2)).limit(n)
+                .collect(Collectors.toMap(x -> x, Function.identity(), (x1, x2) -> x1 + x2));
+        System.out.println(m);
+        Collection<Integer> values = m.values();
+        int count = 0;
+        for (Integer v : values) {
+           count+=v.intValue();
+        }
+
+        for(Map.Entry<Integer,INter>)
+    }
+
+    public static void main(String[] args) {
+        new ThrowCoin().flipCoin(10);
+    }
+}
