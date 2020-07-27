ae992fc542bfcdbb7a6c44854fc0279620a8803b
gbs <gengbushuang@apusapps.com>
2020-07-28 01:54:28 +0800

    单例模式

--- java-test/src/main/java/com/pattern/create/singleton/SingletionLock.java
+++ java-test/src/main/java/com/pattern/create/singleton/SingletionLock.java
@@ -21,4 +21,19 @@ public class SingletionLock {
         }
         return instance;
     }
+
+    //https://github.com/google/guava/issues/3381
+    //初始化情况下(从4次减少到3次)，不需要初始化的情况(从2次减少到1次)
+    public static SingletionLock getInstance1() {
+        SingletionLock tmp = instance;
+        if (tmp == null) {
+            synchronized (SingletionLock.class) {
+                tmp = instance;
+                if (tmp == null) {
+                    instance = tmp = new SingletionLock();
+                }
+            }
+        }
+        return tmp;
+    }
 }
